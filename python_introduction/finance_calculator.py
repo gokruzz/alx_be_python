123da19f25ec8962f599e2a373679a2b580baef0
gokruzz <Johnsonodukoyatemitope@gmail.com>
2025-05-25 17:29:16 +0100

    error rectified

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -8,8 +8,7 @@ expenses = float(input("Enter your total monthly expenses: "))
 monthly_savings = income - expenses
 
 # Project annual savings with 5% interest
-annual_savings = monthly_savings * 12
-projected_savings = annual_savings + (annual_savings * 0.05)
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 # Convert to integers for display
 monthly_savings = int(monthly_savings)
